dac28999ad6d48dd4829005989a62e74f322192d
A7Coder <a7coder@gmail.com>
2023-06-22 12:24:14 +0530

    Added timeout in Scan.py and handled some bugs in XSS and Updated Sql Injection

--- scan.py
+++ scan.py
@@ -1,44 +1,38 @@
-import datetime
 import requests
-import socket
-import time
-import ssl
 import concurrent
 from concurrent.futures import ThreadPoolExecutor
-from urllib.parse import urlparse, urljoin
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-from cipher import weak_cipher_suites
 from collections import deque
 
-start_time = time.time()
-
-def main(url, resp=[]):
+def scan_broken_links(url):
     try:
-        res = requests.get(url, timeout=5)
-        if res.status_code == 200:
-            scan_website(url,resp)
-            check_security_headers(url, resp)
-            check_open_ports(url, resp)
+        resp=[]
+        all_links = get_all_links(url)
+        with ThreadPoolExecutor(max_workers=100) as executor:
+            futures = {executor.submit(check_link_status, link, resp): link for link in all_links}
+            concurrent.futures.wait(futures)
+        return resp
     except:
-        resp.append({"severity":"Critical","target_url": url,"title":"Website Down", "method":"GET", "vulnerable_url": url, "description": f"Host URL {url} is down"})
-    elapsed_time = time.time() - start_time
-    print(len(resp))
-    print(elapsed_time)
+        return "Something went wrong"
 
 parent_dict = {}
 def get_all_links(url):
-    actual_domain=get_host__domain(url)
+
+    actual_domain = get_host__domain(url)
     all_links = set()
-    queue_links=deque([])
-    visited=set()
+    queue_links = deque([])
+    visited = set()
+
     all_links.add(url)
     queue_links.append(url)
+
     while queue_links:
-        url= queue_links.popleft()
+        url = queue_links.popleft()
         if url.endswith('/'):
-            url=url[:-1]
+            url = url[:-1]
         if url not in visited and get_host__domain(url) == actual_domain:
-            response = requests.get(url)
+            response = requests.get(url,timeout=5)
             if not response.status_code == 200:
                 all_links.add(url)
                 visited.add(url)
@@ -48,10 +42,10 @@ def get_all_links(url):
                 href = anchor.get('href')
                 if href and not (href.startswith("mailto:") or href.startswith("javascript:") or href.startswith('tel') or href.startswith('/#') or href.startswith('#')):
                     absolute_url = href
-                    if not (absolute_url.startswith("http") or absolute_url.startswith("https")) :
-                        absolute_url =urljoin(url, href)                 
+                    if not (absolute_url.startswith("http") or absolute_url.startswith("https")):
+                        absolute_url = urljoin(url, href)
                     if absolute_url.endswith('/'):
-                        absolute_url=absolute_url[:-1]
+                        absolute_url = absolute_url[:-1]
                     if absolute_url not in parent_dict:
                         parent_dict[absolute_url] = set()
                     parent_dict[absolute_url].add(url)
@@ -60,129 +54,49 @@ def get_all_links(url):
         visited.add(url)
     return all_links
 
-def check_link_status(link,resp):
+def check_link_status(link, resp):
     try:
-        response = requests.get(link,timeout=2)
-        if response.status_code !=200:
-            resp.append({"severity":"Info","target_url": parent_dict[link],"title":"Broken Links", "method":"GET", "vulnerable_url": link, "description": "Invalid URL with status code {}".format(response.status_code)})
-            return False 
+        response = requests.get(link, timeout=2)
+        if response.status_code != 200:
+            resp.append({"severity": "Info", "target_url": parent_dict[link], "title": "Broken Links", "method": "GET",
+                        "vulnerable_url": link, "description": "Invalid URL with status code {}".format(response.status_code)})
+            return False
         return True
-    except :
-        resp.append({"severity":"Info","target_url": parent_dict[link],"title":"Broken Links", "method":"GET", "vulnerable_url": link, "description": "Invalid URL"})
-        return False
-
-def scan_website(url,resp):
-    try:    
-        all_links = get_all_links(url)
-        broken_links = []
-        valid_links = 0
-        with ThreadPoolExecutor(max_workers=50) as executor:
-            futures = {executor.submit(check_link_status, link,resp): link for link in all_links}
-            for future in concurrent.futures.as_completed(futures):
-                link = futures[future]
-                status_code = future.result()
-                if status_code:
-                    valid_links += 1
-                else:
-                    broken_links.append(link)
-        return all_links, valid_links, broken_links
     except:
-        pass
+        resp.append({"severity": "Info", "target_url": parent_dict[link], "title": "Broken Links",
+                    "method": "GET", "vulnerable_url": link, "description": "Invalid URL"})
+        return False
 
 def get_host__domain(url):
     Domain_name = ''
     x = url.split("/")
-    
-    if(x[0] == "https:" or x[0] == "http:"):
+    if (x[0] == "https:" or x[0] == "http:"):
         x = x[2].split(".")
     else:
         x = x[0].split(".")
-    if(len(x) == 2):
+    if (len(x) == 2):
         Domain_name = x[0]
     else:
         Domain_name = x[1]
     return Domain_name
 
-def check_security_headers(url, resp):
-    response = requests.get(url)
-    headers = response.headers
-    security_headers = [
-        "Strict-Transport-Security",
-        "Content-Security-Policy",
-        "X-Content-Type-Options",
-        "X-Frame-Options",
-        "X-XSS-Protection",
-        "Referrer-Policy",
-    ]
-    for header in security_headers:
-        if not header in headers:
-            resp.append({'severity':'Low','target_url':url,'vulnerable_url':'','title':'Security headers','method':'GET','description':'{} is not present.'.format(header)})
-
-def check_open_port(host, port, resp):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(1)
-        result = s.connect_ex((host, port))
-        if result == 0:
-            try:
-                service = socket.getservbyport(port)
-            except:
-                service = 'Unknown'
-            s.close()
-            if port != 80 and port != 443:
-                return (port, service)
-            elif port == 443:
-                resp.append({'severity':'Info','target_url':host,'vulnerable_url':host,'title':'Open Ports','method':'GET','description':'Port 443 is open and service running on it is : HTTPS'})
-                check_ssl(host, resp)
-            elif port == 80:
-                resp.append({'severity':'Medium','target_url':host,'vulnerable_url':host,'title':'Open Ports','method':'GET','description':'Port 80 is open and service running on it is : HTTP'})
-        s.close()
-
-def check_open_ports(url, resp):
-    parsed_url = urlparse(url)
-    host = parsed_url.netloc
-    ip = socket.gethostbyname(host)
-    with ThreadPoolExecutor(max_workers=500) as executor:
-        tasks = [executor.submit(check_open_port, host, port, resp)
-                 for port in range(1, 65535)]
-        for future in concurrent.futures.as_completed(tasks):
-            result = future.result()
-            if result:
-                resp.append({'severity':'Low','target_url':url,'vulnerable_url':ip,'title':'Open Ports','method':'GET','description':'Port {} is open and service running on it is : {}'.format(result[0], result[1])})
-
-def check_ssl(host, resp):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = host.replace("https://", "").replace("http://", "").split("/")[0]
-    ssl_sock = ssl.create_default_context().wrap_socket(sock, server_hostname=host)
+def check_security_headers(url):
     try:
-        ssl_sock.connect_ex((host, 443))
-        cert = ssl_sock.getpeercert()
-        expiry_date = datetime.datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
-        current_date = datetime.datetime.now()
-        days_to_expiry = (expiry_date - current_date).days
-        ssl_version = ssl_sock.version()
-        cipher_suite = ssl_sock.cipher()
-        
-        if days_to_expiry < 30:
-            resp.append({'severity':'High','target_url':host,'vulnerable_url':host,'title':'SSL/TLS','method':'GET','description':'SSL Certificate is going to expire in {} days.'.format(days_to_expiry)})
-        elif days_to_expiry < 90:
-            resp.append({'severity':'Low','target_url':host,'vulnerable_url':host,'title':'SSL/TLS','method':'GET','description':'SSL Certificate is going to expire in {} days.'.format(days_to_expiry)})
-        else:
-            resp.append({'severity':'Info','target_url':host,'vulnerable_url':host,'title':'SSL/TLS','method':'GET','description':'SSL Certificate is going to expire in {} days.'.format(days_to_expiry)})
-        
-        if ssl_version.startswith(('TLSv1.3')) or ssl_version.startswith(('TLSv1.2')):
-            pass
-        else:
-            resp.append({'severity':'High','target_url':host,'vulnerable_url':host,'title':'SSL/TLS','method':'GET','description':'SSL/TLS version is outdated & current version is {}'.format(ssl_version)})
-
-        if cipher_suite in weak_cipher_suites:
-            resp.append({'severity':'High','target_url':host,'vulnerable_url':host,'title':'SSL/TLS','method':'GET','description':'SSL/TLS is using weak cipher suite: {} & may be vulnerable.'.format(cipher_suite)})
-    except ssl.SSLError as e:
-        resp.append({'severity':'Medium','target_url':host,'vulnerable_url':host,'title':'SSL/TLS','method':'GET','description':'SSL Error: {}'.format(str(e))})
-    except socket.error as e:
-        resp.append({'severity':'Medium','target_url':host,'vulnerable_url':host,'title':'SSL/TLS','method':'GET','description':'Socket Error: {}'.format(str(e))})
-    finally:
-        ssl_sock.close()
-        sock.close()
-
-if __name__ == "__main__":
-    main("http://www.deadlinkcity.com/")
+        resp=[]
+        response = requests.get(url)
+        headers = response.headers
+        security_headers = [
+            "Strict-Transport-Security",
+            "Content-Security-Policy",
+            "X-Content-Type-Options",
+            "X-Frame-Options",
+            "X-XSS-Protection",
+            "Referrer-Policy",
+        ]
+        for header in security_headers:
+            if not header in headers:
+                resp.append({'severity': 'Low', 'target_url': url, 'vulnerable_url': '', 'title': 'Security headers',
+                            'method': 'GET', 'description': '{} is not present.'.format(header)})
+        return resp
+    except:
+        return 'Website is Down'
--- sql_injection.py
+++ sql_injection.py
@@ -1,228 +1,157 @@
-# from random import randint
 import requests
 import concurrent
 from concurrent.futures import ThreadPoolExecutor
-from urllib.parse import urlparse, urljoin,parse_qs,urlencode
+from urllib.parse import urlparse, urljoin, parse_qs, urlencode
 from bs4 import BeautifulSoup
 from scan import get_all_links
 
 def send(resp, data):
-    parent_url,form_url = resp['parent_url'],resp['action']
-    visited=set()
+    parent_url, form_url = resp['parent_url'], resp['action']
+    visited = set()
     visited.add(form_url)
     visited.add(parent_url)
-
-
     if resp['method'] == 'post':
-
-
-        response = requests.post(form_url, data=data, allow_redirects=False)
- 
+        response = requests.post(
+            form_url, data=data, allow_redirects=False, timeout=5)
         while response.status_code == 302:
-            
-            # Extract the redirect URL from the response headers
             redirect_url = response.headers['Location']
             if not (redirect_url.startswith("http") or redirect_url.startswith("https")):
-                            redirect_url = urljoin(parent_url, redirect_url)
-            
-            # print('While Loop ',response.headers['Location'],' ',redirect_url)
+                redirect_url = urljoin(parent_url, redirect_url)
             if redirect_url in visited:
-                # print('PResent')
-                response.status_code=404
+                response.status_code = 404
                 return response
-            
-            # Make a new POST request to the redirect URL
-            response = requests.post(redirect_url, data=data, allow_redirects=False)
+            response = requests.post(
+                redirect_url, data=data, allow_redirects=False, timeout=5)
             visited.add(redirect_url)
-
     else:
-        response = requests.get(form_url, data=data, allow_redirects=False)
+        response = requests.get(form_url, data=data,
+                                allow_redirects=False, timeout=5)
         while response.status_code == 302:
-            # Extract the redirect URL from the response headers
             redirect_url = response.headers['Location']
             if not (redirect_url.startswith("http") or redirect_url.startswith("https")):
-                            redirect_url = urljoin(parent_url, redirect_url)
-            
-            # print('While Loop ',response.headers['Location'],' ',redirect_url)
+                redirect_url = urljoin(parent_url, redirect_url)
             if redirect_url in visited:
-                # print('PResent')
-                response.status_code=404
+                response.status_code = 404
                 return response
-            
-            # Make a new POST request to the redirect URL
-            response = requests.get(redirect_url, data=data, allow_redirects=False)
+            response = requests.get(
+                redirect_url, data=data, allow_redirects=False, timeout=5)
             visited.add(redirect_url)
-
-    # print('Response is ',response,data)
-    # Return the final response
-    # print(response.text)
     return response
 
 def generate_payload():
-    ans=''
-    with open('./payloads.txt','r') as f:
-        ans=f.read()
-    res=ans.split('\n')
+    ans = ''
+    with open('payloads.txt', 'r') as f:
+        ans = f.read()
+    res = ans.split('\n')
     return res
 
 def check_payload(resp):
-    # print('Status Code ',resp.status_code)
-    if resp.status_code ==200:
+    if resp.status_code == 200:
         return True
     return False
 
-
 def run_link_attack(url):
     try:
-        # print('Link Attack ****************',url)
-        payloads=generate_payload()
-        query=urlparse(url).query
-
+        payloads = generate_payload()
+        query = urlparse(url).query
         if query != "":
             for payload in payloads:
-
-                payload = generate_payload()
-                
-                
-                query_payload=query.replace(query[query.find("=")+1:len(query)],payload,1)
-                test=url.replace(query,query_payload,1)
-
-                query_all=url.replace(query,urlencode({x:payload for x in parse_qs(query)}))
-
-                _respon=requests.get(test)
-
-                if check_payload(_respon) or check_payload(requests.get(query_all)):
-                        
-                    return {'severity':'Medium','target_url':url,'vulnerable_url':_respon.url,'title':'SQL-INJECTION','method':'GET','description':f'SQL-INJECTION vulnerability found at vulnerable url with payload {payload}'}
+                query_payload = query.replace(
+                    query[query.find("=")+1:len(query)], payload, 1)
+                test = url.replace(query, query_payload, 1)
+                query_all = url.replace(query, urlencode(
+                    {x: payload for x in parse_qs(query)}))
+                _respon = requests.get(test, timeout=5)
+                if check_payload(_respon) or check_payload(requests.get(query_all, timeout=5)):
+                    return {'severity': 'Medium', 'target_url': url, 'vulnerable_url': _respon.url, 'title': 'SQL-INJECTION', 'method': 'GET', 'description': f'SQL-INJECTION vulnerability found at vulnerable url with payload {payload}'}
         else:
             return None
-    except:
+    except Exception as e:
         return None
-
-
+    
 def run_form_attack(resp):
-    payloads=generate_payload()
-    # print('Response is ',resp)
+    payloads = generate_payload()
     for payload in payloads:
-
         keys = {}
-        
         for item in resp['field']:
             keys[item] = payload
-
-        # print('Payloads are ******************')
         if 'submit' in resp:
-            keys[resp['submit'][0]]=resp['submit'][1]
-
-        # print('Form Attack ****************',keys)
-
-        req=send(resp,keys)
-
-        # print('Response Text is ')
-        # print(req.text)
+            keys[resp['submit'][0]] = resp['submit'][1]
+        req = send(resp, keys)
         if check_payload(req):
-            # print('Content ********************* are ',req.text)
-            # print('Payload is ',payload)
-            return (True,keys)
-
-    return (False,keys)
-
+            return (True, keys)
+    return (False, keys)
 
 def run_sql(url):
     try:
-        res = requests.get(url,timeout=5)
+        res = requests.get(url, timeout=5)
         bsObj = BeautifulSoup(res.content, "html.parser")
         forms = bsObj.find_all("form", method=True)
-        
-        result=[]
-        
+        result = []
         for form in forms:
-            
             resp = {}
             try:
-
                 if (form['action'].startswith('https') or form['action'].startswith('http')):
                     resp["action"] = form["action"]
                 else:
                     resp["action"] = urljoin(url, form["action"])
-
             except KeyError:
-                
                 resp["action"] = res.url
-
             if form["method"].lower().strip() == "post":
                 resp['method'] = 'post'
             else:
                 resp['method'] = 'get'
-
             for key in form.find_all(["input", "textarea"]):
                 try:
-                    if  key.has_attr('name') and key.name == 'input'  and key['type']=='submit' :
-                        
-                        resp['submit']=(key['name'],key['value'])
+                    if key.has_attr('name') and key.name == 'input' and key['type'] == 'submit':
+
+                        resp['submit'] = (key['name'], key['value'])
 
                     elif key.has_attr('name') and ((key.name == 'textarea') or (key.name == 'input')):
-                        
+
                         if 'field' not in resp:
                             resp['field'] = set()
-                        
+
                         resp['field'].add(key['name'])
-                    
-
                 except Exception as e:
-                    # print('Excepiton',e,key)
-                    # print(key.has_attr('name'))
                     continue
-
             try:
-                resp['parent_url']=url
-                # print('******Respponse is ',resp)
-                ans =run_form_attack(resp)
-                # print('Ans is ',ans)
+                resp['parent_url'] = url
+                ans = run_form_attack(resp)
                 if ans[0]:
-
-                    result.append({'severity':'High','target_url':url,'vulnerable_url':resp['action'],'title':'SQL-INJECTION','method':resp['method'].upper(),'description':f"SQL-INJECTION vulnerability found at vulnerable url with payload {ans[1]}"})
+                    result.append({'severity': 'High', 'target_url': url, 'vulnerable_url': resp['action'], 'title': 'SQL-INJECTION', 'method': resp['method'].upper(
+                    ), 'description': f"SQL-INJECTION vulnerability found at vulnerable url with payload {ans[1]}"})
             except Exception as e:
-                # print('Enter exception ' ,e)
                 continue
-        
         return result
-    except :
+    except:
         return []
-    
-def run_both_attack(url):
-    
-    return (run_sql(url),run_link_attack(url))
 
+
+def run_both_attack(url):
+    return (run_sql(url), run_link_attack(url))
 
 def sql_injection(url):
-
     try:
-        
         res = requests.get(url, timeout=5)
-        
-        resp=[]
+        resp = []
         if res.status_code == 200:
-            all_links=get_all_links(url)
-            print('All links fetched****************')
+            all_links = get_all_links(url)
             with ThreadPoolExecutor(max_workers=100) as executor:
-
-                futures = [executor.submit(run_both_attack, link) for link in all_links]
-            
+                futures = [executor.submit(run_both_attack, link)
+                           for link in all_links]
                 for future in concurrent.futures.as_completed(futures):
-                    
                     ans = future.result()
-
                     if ans and ans[0]:
                         resp.append(ans[0])
-                    if len(ans)==2 and ans[1]:
+                    if len(ans) == 2 and ans[1]:
                         resp.append(ans[1])
-
-    
             return resp
     except Exception as e:
         return f'Website is Down {e}'
 
 if __name__ == '__main__':
-
-    print(sql_injection('http://testphp.vulnweb.com'))
+    a = sql_injection('http://testphp.vulnweb.com')
+    print('Ans is ',)
+    print(a)
+    print('Length ', len(a))
